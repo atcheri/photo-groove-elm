a5007ef75d9cd60c4f4ae6c634610ebfc601e356
Atch <atcheri@laposte.net>
2022-09-29 01:18:47 +0200

    feat: model as a record

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,16 +4,55 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+initialModel : { photos : List { url : String }, selectedUrl : String }
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        , { url = "4.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+viewThumbnail : String -> { a | url : String } -> Html.Html msg
+viewThumbnail selectedUrl thumb =
+    if selectedUrl == thumb.url then
+        img
+            [ src (urlPrefix ++ thumb.url)
+            , classList [ ( "selected", selectedUrl == thumb.url ) ]
+            ]
+            []
+
+    else
+        img
+            [ src (urlPrefix ++ thumb.url) ]
+            []
+
+
+view : { photos : List { url : String }, selectedUrl : String } -> Html.Html msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            (List.map
+                (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
 
+main : Html.Html msg
 main =
-    view "no model yet"
+    view initialModel
