8a39ed783541a6a1229aee5bb353baf804cfb74a
Atch <atcheri@laposte.net>
2022-10-08 14:22:56 +0200

    test: writing first test for photoDecoder

---
+++ tests/PhotoGrooveTests.elm
@@ -0,0 +1,19 @@
+module PhotoGrooveTests exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Json.Decode exposing (decodeString)
+import PhotoGroove
+import Test exposing (..)
+
+
+decoderTest : Test
+decoderTest =
+    test "title default to (untitled)" <|
+        \_ ->
+            """
+                {"url": "fruits.com", "size": 5}
+            """
+                |> decodeString PhotoGroove.photoDecoder
+                |> Result.map .title
+                |> Expect.equal (Ok "(untitled)")
