0c23bfce6a7a9fc27144f28868ee150dc24f8985
Atch <atcheri@laposte.net>
2022-10-10 13:24:37 +0200

    test: first test array with slid messages

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -4,8 +4,9 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Json.Decode as Decode exposing (decodeValue)
 import Json.Encode as Encode
-import PhotoGroove
+import PhotoGroove exposing (..)
 import Test exposing (..)
+import Tuple
 
 
 decoderTest : Test
@@ -19,3 +20,23 @@ decoderTest =
                 |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
+
+
+sliders : Test
+sliders =
+    describe "Slider sets the desired field in the model"
+        [ testSlider "SlidHue" SlidHue .hue
+        , testSlider "SlidRipple" SlidRipple .ripple
+        , testSlider "SlidNoise" SlidNoise .noise
+        ]
+
+
+testSlider : String -> (Int -> Msg) -> (Model -> Int) -> Test
+testSlider description toMsg amountFromModel =
+    fuzz int description <|
+        \amount ->
+            initialModel
+                |> update (toMsg amount)
+                |> Tuple.first
+                |> amountFromModel
+                |> Expect.equal amount
